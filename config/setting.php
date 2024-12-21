d75607d5d317307854d65d28d4c6525340614951
Irfan Ghuori <42720620+IrfanGhuori@users.noreply.github.com>
2024-12-21 09:20:45 +0500

    Update setting.php

--- config/setting.php
+++ config/setting.php
@@ -1,17 +1,25 @@
 <?php
+// Database credentials
 $db_username = "root";
 $db_password = "";
-$db_hostname = "localhost"; 
+$db_hostname = "localhost";
+$db_name = "gtpcom_desain";
 
-//connection to the database
-$db_handle = mysql_connect($db_hostname, $db_username, $db_password) 
- or die("Unable to connect to MySQL");
-
-//select a database to work with
-$db_selected = mysql_select_db("gtpcom_desain",$db_handle) 
-  or die("Could not select database");
-
+// Site configuration
 $base_site_name = "cm";
 $base_url = "localhost/".$base_site_name;
 $base_name_site = "Desain";
+
+try {
+    // Create PDO connection
+    $dsn = "mysql:host=$db_hostname;dbname=$db_name;charset=utf8mb4";
+    $db_handle = new PDO(dsn: $dsn, username: $db_username, password: $db_password);
+    
+    // Set error mode to throw exceptions
+    $db_handle->setAttribute(attribute: PDO::ATTR_ERRMODE, value: PDO::ERRMODE_EXCEPTION);
+    $db_handle->setAttribute(attribute: PDO::ATTR_DEFAULT_FETCH_MODE, value: PDO::FETCH_ASSOC);
+    
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
 ?>
